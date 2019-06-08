6d6edab3c0d8382ff8e0097a8339429850f49df9
AJ Green <ajgreen@andrews-macbook.attlocal.net>
2019-06-08 19:16:27 -0400

    Adding images to main

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,39 @@ import 'package:flame/util.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/gestures.dart';
 import 'game/fly_game.dart';
+import 'package:flame/flame.dart';
 
 Future main() async {
-
   // Setup the flame utility
   Util flameUtil = Util();
   await flameUtil.fullScreen();
   await flameUtil.setOrientation(DeviceOrientation.portraitUp);
 
+  // Move this elsewhere
+  Flame.images.loadAll(<String>[
+    'bg/backyard.png',
+    'flies/agile-fly-1.png',
+    'flies/agile-fly-2.png',
+    'flies/agile-fly-dead.png',
+    'flies/drooler-fly-1.png',
+    'flies/drooler-fly-2.png',
+    'flies/drooler-fly-dead.png',
+    'flies/house-fly-1.png',
+    'flies/house-fly-2.png',
+    'flies/house-fly-dead.png',
+    'flies/hungry-fly-1.png',
+    'flies/hungry-fly-2.png',
+    'flies/hungry-fly-dead.png',
+    'flies/macho-fly-1.png',
+    'flies/macho-fly-2.png',
+    'flies/macho-fly-dead.png',
+  ]);
+
   // Create the Game and run the app
   FlyGame game = FlyGame();
   runApp(game.widget);
-  
+
   TapGestureRecognizer tapGestureRecognizer = TapGestureRecognizer();
   tapGestureRecognizer.onTapDown = game.onTapDown;
   flameUtil.addGestureRecognizer(tapGestureRecognizer);
 }
-
